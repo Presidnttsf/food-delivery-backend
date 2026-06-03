9047ead3537ea204c8db84d541a4cb5b73518d8f
YourUsername <presidnttsf@gmail.com>
2026-06-03 18:19:10 +0530

    check

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+
+//connect with database
 const connectDB = async () => {
   const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/food-delivery";
 
