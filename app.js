89fe46760a5f7ec7289417227a15e2db5b291247
YourUsername <presidnttsf@gmail.com>
2026-06-03 18:39:30 +0530

    cors integrated

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const orderRoutes = require("./routes/orderRoutes");
 const { errorHandler, notFound } = require("./middleware/errorHandler");
 
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
