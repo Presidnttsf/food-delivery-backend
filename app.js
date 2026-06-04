76ceb337e9d374d811cc04c7a7bd4b0e582e82c9
YourUsername <presidnttsf@gmail.com>
2026-06-04 18:34:56 +0530

    app.js images fixed

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const menuRoutes = require("./routes/menuRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const { errorHandler, notFound } = require("./middleware/errorHandler");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
-const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +14,12 @@ app.use(express.urlencoded({ extended: false }));
 // Health check
 app.get("/health", (req, res) => res.json({ status: "ok" }));
 
+// Static Images
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "assets/images"))
+);
+
 // API routes
 app.use("/menu", menuRoutes);
 app.use("/orders", orderRoutes);
@@ -20,7 +27,7 @@ app.use("/orders", orderRoutes);
 // 404 handler
 app.use(notFound);
 
-// Centralized error handler (must be last)
+// Centralized error handler
 app.use(errorHandler);
 
 module.exports = app;
